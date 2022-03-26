9b15a552c32fab02b1bacc1341b0cb9feefaa439
spookyorange <korkmazcoltu@mail.ru>
2022-03-26 14:25:47 +0300

    initial commit

---
+++ db/migrate/20220326112512_create_recipes.rb
@@ -0,0 +1,10 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.text :about
+
+      t.timestamps
+    end
+  end
+end
