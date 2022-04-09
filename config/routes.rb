e9cbdfb3fe9d601575af679c966b3f5d4a6c84fe
spookyorange <korkmazcoltu@mail.ru>
2022-04-09 14:02:21 +0300

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "recipes#index"
-  resources :recipes
+  root 'recipes#index'
+  resources :recipes do
+    resources :ingredients, only: [ :create, :update, :destroy ]
+    resources :steps, only: [ :create, :update, :destroy ]
+  end
+
+  resources :profiles, except: :destroy
 end
