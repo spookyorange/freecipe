84a51038b453a373089566dc0c1e824e5b507da3
spookyorange <korkmazcoltu@mail.ru>
2022-04-11 15:27:04 +0300

    seeds fixed for sure

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create([
-              { email: 'dasfa@fadsfas', encrypted_password: '12345678' },
-              { email: 'haha@fadsfas', encrypted_password: '12345678' }
+User.create(email: 'dasfa@fadsfas', password: '12345678')
+User.create(email: 'haha@fadsfas', password: '12345678')
 
-            ])
+Profile.create(user: User.first, username: 'cool dude')
+Profile.create(user: User.second, username: 'not cool dude')
 
-Profile.create([
-                 { user: User.first, username: 'cool dude' },
-                 { user: User.second, username: 'not cool dude' }
-
-               ])
-
-Recipe.create([
-                { profile: Profile.first,
-                  title: 'Pasta with Cream',
-                  about: 'It is a pasta that has been made with cream and it is delicious' },
-                { profile: Profile.second,
-                  title: 'How to install linux',
-                  about: 'I am going to show you how to make a live boot usb and
-how to use it to install a Linux distro' }
-              ])
+Recipe.create(profile: Profile.first,
+              title: 'Pasta with Cream',
+              about: 'It is a pasta that has been made with cream and it is delicious')
+Recipe.create(profile: Profile.second,
+              title: 'How to install linux',
+              about: 'I am going to show you how to make a live boot usb and
+how to use it to install a Linux distro')
