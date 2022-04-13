f9a184a114f1bc6b42c182ae7b875c48ede22e3d
spookyorange <korkmazcoltu@mail.ru>
2022-04-13 16:20:55 +0300

    ingredients controller partly implemented

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,48 @@
 class IngredientsController < ApplicationController
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient.recipe = @recipe
+
+    if !user_signed_in? || current_user.profile.nil? ||
+       @recipe.profile != current_user.profile
+      redirect_to @recipe
+    else
+      if @ingredient.save
+        flash[:notice] = 'Ingredient saved successfully'
+        redirect_to @recipe
+      else
+        flash[:alert] = 'Something went wrong'
+        redirect_to @recipe
+      end
+    end
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @recipe = @ingredient.recipe
+
+    if @ingredient.update(ingredient_params)
+      redirect_to @recipe
+    else
+      render @recipe
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @recipe = @ingredient.recipe
+
+    @ingredient.destroy
+    redirect_to @recipe
+
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :quantity)
+  end
+
 end
