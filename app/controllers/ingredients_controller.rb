43ced81bc17c0a8336884099a3c44feeaca4c09a
spookyorange <korkmazcoltu@mail.ru>
2022-04-15 13:37:35 +0300

    authenticate owner before action implemented

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,21 +1,25 @@
 class IngredientsController < ApplicationController
+  before_action :authenticate_owner
+
+  def authenticate_owner
+    if !user_signed_in? || current_user.profile.nil? ||
+      Recipe.find(params[:recipe_id]).profile != current_user.profile
+      redirect_to @recipe
+      flash[:alert] = 'This is not your Recipe!'
+    end
+  end
 
   def create
     @ingredient = Ingredient.new(ingredient_params)
     @recipe = Recipe.find(params[:recipe_id])
     @ingredient.recipe = @recipe
 
-    if !user_signed_in? || current_user.profile.nil? ||
-       @recipe.profile != current_user.profile
-      redirect_to @recipe
-    else
-      if @ingredient.save
+    if @ingredient.save
         flash[:notice] = 'Ingredient saved successfully'
         redirect_to @recipe
-      else
-        flash[:alert] = 'Something went wrong'
-        redirect_to @recipe
-      end
+    else
+      flash[:alert] = 'Something went wrong'
+      redirect_to @recipe
     end
   end
 
