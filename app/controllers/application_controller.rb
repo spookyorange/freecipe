504346d1aa1299d6efcc1fac300e751169a4ade6
spookyorange <korkmazcoltu@mail.ru>
2022-04-18 14:58:47 +0300

    user_signed_in? insted of short circuiting current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_profile, except: :destroy
   def authenticate_profile
-    if current_user && current_user.profile.nil?
+    if user_signed_in? && current_user.profile.nil?
       redirect_to new_profile_path
     end
   end
