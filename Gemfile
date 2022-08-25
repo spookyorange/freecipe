f74d6badace35bedaa27f6e7b70abedf86058167
spookyorange <korkmazcoltu@mail.ru>
2022-08-25 13:22:16 +0300

    changed db to postgres from sqlite3

--- Gemfile
+++ Gemfile
@@ -9,15 +9,9 @@ gem 'rails', '~> 7.0', '>= 7.0.3.1'
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
-# Use sqlite3 as the database for Active Record
+# Use pg as the database for Active Record
+gem "pg", "~> 1.4"
 
-group :development, :test do
-  gem "sqlite3", "~> 1.4"
-end
-
-group :production do
-  gem 'pg', '~> 1.3', '>= 1.3.5'
-end
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,20 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_09_102042) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
-    t.integer "recipe_id", null: false
+    t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -33,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_09_102042) do
     t.text "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "profile_id"
+    t.bigint "profile_id"
     t.index ["profile_id"], name: "index_recipes_on_profile_id"
   end
 
@@ -42,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_09_102042) do
     t.text "description"
     t.integer "time_consumption"
     t.integer "order"
-    t.integer "recipe_id", null: false
+    t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_steps_on_recipe_id"
