e3c0a96feda3c2ee67d33a33c40c8f7fb764559c
spookyorange <korkmazcoltu@mail.ru>
2022-10-03 15:49:22 +0300

    "todo: flash die" implemented

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,5 @@
 import {Application} from "@hotwired/stimulus"
 
-/* TODO: Flashes should die or get hidden class after the animation finishes */
-
 const application = Application.start()
 
 // Configure Stimulus development experience
